c9cdbb6effe43b57554b2dde8fb666c8078cbde9
Pasha Myndzul <51452458+PashaMyndzul@users.noreply.github.com>
2019-06-28 08:51:08 +0300

    correct errors

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 const value = 8;
 function convert (usd){
     const convert=usd*value;
-    return `при курсі 8-- ${usd} дол= ${convert} грн`;
+    return `at course 8--> ${usd} usd= ${convert} uah`;
 }
 console.log(convert(25));
 
 //Task#2
 function reverse(str) {
-    const result =str.split(``).reverse().join(``);
+    const result = str.split(``).reverse().join(``);
     return `Rezult: ${result}`;
 }
 console.log(reverse("Pasha"));
@@ -16,41 +16,47 @@ console.log(reverse("Pasha"));
 //Task#3
 function printStairs(n) {
     let  icon= "";
-    for(let i =0; i < n; i++){
-    console.log(icon+=':)');
+    for(let i = 0; i < n; i++){
+    console.log(icon +=':)');
     }
  }
  printStairs(8);
 
  //task#4
- function sumRange(start,end){
-    let sumRange=0;
-    for (let i=start; i<=end;i++){
-        sumRange+=i;
-         console.log(sumRange);}
-        return console.log('Rezult:'+sumRange);
+ function sumRange (start, end) {
+    let sumRange = 0;
+    for (let i = start; i <= end; i++){
+        sumRange += i;
+        }
+        return sumRange
  }
- sumRange (2,4);
- sumRange (-1,3);
+ console.log ('rezult:' + sumRange (2,4));
+ console.log ('rezult:' + sumRange (-1,3));
  //task#5
  function min(a,b,c){
-    if ((a==b)&&(b==c)){
-         console.log(`Введені значення рівні:)`);}
-    else if ((a<=b)&&(a<c)) {
-         console.log(`min число ${a}`);}
-    else if ((b<=a)&&(b<c)){
-        console.log(`min число ${b}`);}
-    else if ((c<a)&&(c<b)){
-        console.log(`min число ${c}`);}
-    else {console.log(`Невірний формат введених даних :(`);}
- }
+    let min = a;
+   
+    for(let i = 0; i < arguments.length; i++){
+       if(min > arguments[i]) {
+        min = arguments[i];
+       } else {}
+            
+            
+} 
+
+    return console.log(min);
+    } 
+    
+   
+
+    
  min(10,5,11);
  min(3,8,4);
  //task#6
  function printPiramid(n) {
     let  icon= "";
-    for(let i =0; i < n; i++){
-     let x=((n*2)-(i*2))/2;
+    for(let i = 0; i < n; i++){
+     let x=((n * 2) - (i * 2)) / 2;
     //let a="";                 //Інший метод вирішення завдання;
     // for (let c=0;c<x;c++){
     //    a+=" ";
@@ -69,8 +75,7 @@ function firstAndLastToUpper(str){
 firstAndLastToUpper(`abc`);
 //task#8
 function cursorCheck(str) {
-   // let a = str;
-    for( let i =0; i < str.length; i++){
+    for( let i = 0; i < str.length; i++){
     if(~str.toLowerCase().indexOf("ironman") || ~str.toLowerCase().indexOf("cursor") || ~str.toLowerCase().indexOf("ostap")){
         return true;
     } else {
@@ -82,21 +87,30 @@ console.log(cursorCheck("Hello I am OstaP"));
 console.log(cursorCheck("Superman is here"));
 //task9
 function toUpperCase(str){
-    let toUpperChar='';
-    for( let i =0; i < str.length; i++){
-    let toUpperChar1 = String.fromCharCode(str.charCodeAt(i)-32);
-    toUpperChar += toUpperChar1;
-    }
+    let toUpperChar = "";
+    for(let i = 0; i < str.length; i++){
+        if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122)  
+        {
+            let toUpperChar1 = String.fromCharCode(str.charCodeAt(i)-32);
+            toUpperChar += toUpperChar1;
+        } else {
+            toUpperChar += str[i];
+            return toUpperChar;
+        }
+}
     console.log(toUpperChar);
 }
 toUpperCase('abc');
 //task10
 function removeDuplicationLetters(str){
-        let noDublicate='';
-        for(let i=0; i<str.length; i++){
-            if((noDublicate.toUpperCase().indexOf(str[i])==-1)&&(noDublicate.toLowerCase().indexOf(str[i])==-1)){
-                noDublicate += str[i];
-            }   
+        let noDublicate = '';
+        for(let i = 0; i < str.length; i++){
+            if((noDublicate.toUpperCase().indexOf(str[i]) == -1)&&
+                (noDublicate.toLowerCase().indexOf(str[i]) ==-1))
+                {
+                noDublicate += str[i].trim();
+                
+                }   
         }
             return noDublicate;
     }
@@ -108,10 +122,9 @@ function fibonacci(n){
         let c = a + b;
         a=b;
         b=c;
-        //console.log(b);
     }
-    console.log(n+" "+ "позиція в числі фібоначчі-->"+" " + b);
-    return `${n} позиція в числі фібоначчі--> ${b}`;
+    console.log(n+" "+ "position among Fibonacci numbers-->"+" " + b);
+    return `${n} position among Fibonacci numbers--> ${b}`;
 }
 fibonacci(3);
 fibonacci(5);
